86fe1da99952a420d520ad3e5f8d91a32e711ecd
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-10-06 12:38:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
 """
 Цель: написать простейшего телеграм-бота, используя асинхронные функции.
 
-Подготовка:
-Выполните все действия представленные в предыдущих видео модуля, создав и подготовив
-Telegram-бот для дальнейших заданий.
-
-Задача "Бот поддержки (Начало)":
-
-К коду из подготовительного видео напишите две асинхронные функции:
-
-start(message) - печатает строку в консоли 'Привет! Я бот помогающий твоему здоровью.' .
-Запускается только когда написана команда '/start' в чате с ботом. (используйте
-соответствующий декоратор)
-
-all_massages(message) - печатает строку в консоли 'Введите команду /start, чтобы начать
-общение.'. Запускается при любом обращении не описанном ранее. (используйте соответствующий
-декоратор)
-
+Задача "Он мне ответил!":
+Измените функции start и all_messages так, чтобы вместо вывода в консоль строки отправлялись
+в чате телеграм.
 Запустите ваш Telegram-бот и проверьте его на работоспособность.
 
-Пример результата выполнения программы:
 
-Ввод в чат Telegram:
-
-Хэй!
-/start
-
-Вывод в консоль:
-
-Updates were skipped successfully.
-Введите команду /start, чтобы начать общение.
-Привет! Я бот помогающий твоему здоровью.
 """
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -42,19 +18,19 @@ bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(text=['Urban'])
-async def urban_massage(message):
-    print('Urban message')
+# @dp.message_handler(text=['Urban'])
+# async def urban_massage(message):
+#     print('Urban message')
 
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_message(message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
