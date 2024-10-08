4e4e5b8e58d87003dde4c85c92a95b1db195774c
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-10-08 21:45:54 +0300

    Add files via upload

---
+++ telegram_bot_13_5.py
@@ -0,0 +1,102 @@
+"""
+Задача "Меньше текста, больше кликов":
+Необходимо дополнить код предыдущей задачи, чтобы вопросы о параметрах тела для расчёта калорий
+выдавались по нажатию кнопки.
+
+Измените massage_handler для функции set_age.
+Теперь этот хэндлер будет реагировать на текст 'Рассчитать', а не на 'Calories'.
+
+Создайте клавиатуру ReplyKeyboardMarkup и 2 кнопки KeyboardButton на ней со следующим текстом:
+'Рассчитать' и 'Информация'. Сделайте так, чтобы клавиатура подстраивалась под размеры интерфейса
+устройства при помощи параметра resize_keyboard.
+
+Используйте ранее созданную клавиатуру в ответе функции start, используя параметр reply_markup.
+В итоге при команде /start у вас должна присылаться клавиатура с двумя кнопками.
+При нажатии на кнопку с надписью 'Рассчитать' срабатывает функция set_age с которой начинается
+работа машины состояний для age, growth и weight.
+"""
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = "7817004865:AAFBqVj3Xa3maRLafdwpfAYqoBnVr0IBUdw"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    gender = State()
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    button_1 = KeyboardButton(text='Рассчитать')
+    button_2 = KeyboardButton(text='Информация')
+    keyboard = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        input_field_placeholder='Введите данные здесь')
+    keyboard.add(button_1)
+    keyboard.add(button_2)
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=keyboard)
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_gender(message):
+    await message.answer('Назовите свой пол: men/women')
+    await UserState.gender.set()
+
+
+@dp.message_handler(state=UserState.gender)
+async def set_age(message, state):
+    await state.update_data(gender=message.text.lower())
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост в см:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))
+    await message.answer('Введите свой вес в кг:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+    if data['gender'] == 'women':
+        res_for_women = (
+                10 * data['weight']
+                + 6.25 * data['growth']
+                - 5 * data['age']
+                - 161
+        )
+        await message.answer(f'Ваша норма калорий {res_for_women}')
+    if data['gender'] == 'men':
+        res_for_men = (
+                10 * data['weight']
+                + 6.25 * data['growth']
+                - 5 * data['age']
+                + 5
+        )
+        await message.answer(f'Ваша норма калорий {res_for_men}')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
