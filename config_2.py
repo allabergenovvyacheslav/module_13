ca77af2c754eee5111ed8df9575bb85530bbd6b5
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-10-10 12:09:22 +0300

    Add files via upload

---
+++ config_2.py
@@ -0,0 +1,14 @@
+
+API = ""
+
+price_parquet_flooring_S = 4060
+price_large_format_parquet_S = 5550
+price_solid_wood_parquet_S = 5940
+
+price_parquet_flooring_N = 4060
+price_large_format_parquet_N = 5550
+price_solid_wood_parquet_N = 5940
+
+price_parquet_flooring_R = 4060
+price_large_format_parquet_R = 5550
+price_solid_wood_parquet_R = 5940
