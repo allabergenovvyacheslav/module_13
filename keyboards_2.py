83c455bcc5e81c0d037f395062d93948a06eefb6
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-10-12 17:47:49 +0300

    Update keyboards_2.py

--- keyboards_2.py
+++ keyboards_2.py
@@ -1,4 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 start_kb = ReplyKeyboardMarkup(
     keyboard=[
@@ -7,4 +8,25 @@ start_kb = ReplyKeyboardMarkup(
             KeyboardButton(text="О компании Мир паркета")
         ]
     ], resize_keyboard=True
+)
+
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Штучный паркет дуб селект", callback_data='parquet_flooring_S')],
+        [InlineKeyboardButton(text="Широкоформатный паркет дуб селект", callback_data='large_format_parquet_S')],
+        [InlineKeyboardButton(text="Массивная доска дуб селект", callback_data='solid_wood_parquet_S')],
+        [InlineKeyboardButton(text="Штучный паркет дуб натур", callback_data='parquet_flooring_N')],
+        [InlineKeyboardButton(text="Широкоформатный паркет дуб натур", callback_data='large_format_parquet_N')],
+        [InlineKeyboardButton(text="Массивная доска дуб натур", callback_data='solid_wood_parquet_N')],
+        [InlineKeyboardButton(text="Штучный паркет дуб рустик", callback_data='parquet_flooring_R')],
+        [InlineKeyboardButton(text="Широкоформатный паркет дуб рустик", callback_data='large_format_parquet_R')],
+        [InlineKeyboardButton(text="Массивная доска дуб рустик", callback_data='solid_wood_parquet_R')],
+        [InlineKeyboardButton(text="Другие предложения", callback_data='oter')]
+    ]
+)
+
+buy_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Купить', url='http://mir-parketa.spb.ru')]
+    ]
 )
